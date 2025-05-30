9a7659867e32cee4f28aa6cc5f53d3fa57db1571
Revathi Prasad <prasadrevathi.2021@gmail.com>
2025-05-29 20:01:16 -0500

    Initial Commit

---
+++ PupillometryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PupillometryApp
+//
+//  Created by Revathi Prasad on 29/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PupillometryAppTests/PupillometryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PupillometryAppTests.swift
+//  PupillometryAppTests
+//
+//  Created by Revathi Prasad on 29/05/25.
+//
+
+import Testing
+@testable import PupillometryApp
+
+struct PupillometryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
